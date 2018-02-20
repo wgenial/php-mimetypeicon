f8a88e41ed3d5c4b6572ffadd8c18ad9f14d3898
Giovani Generali <giovani.generali@gmail.com>
2018-02-20 11:32:54 -0300

    add fallback icon

--- src/PHPMimeTypeIcon.php
+++ src/PHPMimeTypeIcon.php
@@ -12,8 +12,9 @@
       $mime = new MimeDbRepository();
       $file_extension = pathinfo($file, PATHINFO_EXTENSION);
       $file_mimetype = $mime->findType($file_extension);
+      $file_mimetype = !empty($file_mimetype) ? preg_replace("/\//","-", strtolower($file_mimetype)) : "application-octet-stream";
       $icon_extension = (($size == "scalable") ? ".svg" : ".png");
-      $icon = $icons_assets_path ."/". $size ."/".  preg_replace("/\//","-", $file_mimetype) . $icon_extension;
+      $icon = $icons_assets_path ."/". $size ."/". $file_mimetype . $icon_extension;
       return $icon;
     }
 
