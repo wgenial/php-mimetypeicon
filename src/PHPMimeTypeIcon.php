3be430cce532219d7dbc63acf1359d945c78e5c4
Giovani Generali <giovani.generali@gmail.com>
2018-02-24 16:55:03 -0300

    bugfix

--- src/PHPMimeTypeIcon.php
+++ src/PHPMimeTypeIcon.php
@@ -11,8 +11,8 @@
       $icons_assets_path = "/vendor/wgenial/php-mimetypeicon/icons";
       $mime = new MimeDbRepository();
       $file_extension = pathinfo($file, PATHINFO_EXTENSION);
-      $file_mimetype = $mime->findType($file_extension);
-      $file_mimetype = !empty($file_mimetype) ? preg_replace("/\//","-", strtolower($file_mimetype)) : "application-octet-stream";
+      $file_mimetype = $mime->findType(strtolower($file_extension));
+      $file_mimetype = !empty($file_mimetype) ? preg_replace("/\//","-", $file_mimetype) : "unknown";
       $icon_extension = (($size == "scalable") ? ".svg" : ".png");
       $icon = $icons_assets_path ."/". $size ."/". $file_mimetype . $icon_extension;
       return $icon;
