6d666939f82c22492f00c9a9f421e95379b3ec15
Giovani Generali <giovani.generali@gmail.com>
2018-11-05 17:51:32 -0200

    update pkg php-mimetyper and readme

--- README.md
+++ README.md
@@ -31,21 +31,14 @@ echo "<img style='width:180px;height:180px;' src='". PHPMimeTypeIcon::getURI('ht
 ## ToDo
 **Extension** | **MIME Type**
 --- | ---
-~~.doc~~ | ~~application/msword~~
-~~.docx~~ | ~~application/vnd.openxmlformats-officedocument.wordprocessingml.document~~
-~~.dotx~~ | ~~application/vnd.openxmlformats-officedocument.wordprocessingml.template~~
 .docm | application/vnd.ms-word.document.macroEnabled.12
 .dotm | application/vnd.ms-word.template.macroEnabled.12
-~~.xls~~ | ~~application/vnd.ms-excel~~
 .xlt |  application/vnd.ms-excel
 .xla |  application/vnd.ms-excel
-~~.xlsx~~ | ~~application/vnd.openxmlformats-officedocument.spreadsheetml.sheet~~
 .xltx | application/vnd.openxmlformats-officedocument.spreadsheetml.template
-~~.xlsm~~ | ~~application/vnd.ms-excel.sheet.macroEnabled.12~~
 .xltm | application/vnd.ms-excel.template.macroEnabled.12
 .xlam | application/vnd.ms-excel.addin.macroEnabled.12
 .xlsb | application/vnd.ms-excel.sheet.binary.macroEnabled.12
-~~.ppt~~ |  ~~application/vnd.ms-powerpoint~~
 .pot |  application/vnd.ms-powerpoint
 .pps |  application/vnd.ms-powerpoint
 .ppa |  application/vnd.ms-powerpoint
--- src/PHPMimeTypeIcon.php
+++ src/PHPMimeTypeIcon.php
@@ -1,21 +1,25 @@
 <?php
- 
-  namespace WGenial\PHPMimeTypeIcon;
+namespace WGenial\PHPMimeTypeIcon;
 
-  use MimeTyper\Repository\MimeDbRepository;
+use MimeTyper\Repository\MimeDbRepository;
 
-  class PHPMimeTypeIcon {
+class PHPMimeTypeIcon {
 
-    static public function getURI($file, $size)
-    {
-      $icons_assets_path = "/vendor/wgenial/php-mimetypeicon/icons";
-      $mime = new MimeDbRepository();
-      $file_extension = pathinfo($file, PATHINFO_EXTENSION);
-      $file_mimetype = $mime->findType(strtolower($file_extension));
-      $file_mimetype = !empty($file_mimetype) ? preg_replace("/\//","-", $file_mimetype) : "unknown";
-      $icon_extension = (($size == "scalable") ? ".svg" : ".png");
-      $icon = $icons_assets_path ."/". $size ."/". $file_mimetype . $icon_extension;
-      return $icon;
-    }
+  static public function getURI($file, $size)
+  {
+    $icons_assets_path = "/vendor/wgenial/php-mimetypeicon/icons";
 
+    $mime = new MimeDbRepository();
+
+    $file_extension = pathinfo($file, PATHINFO_EXTENSION);
+
+    $file_mimetype = $mime->findType(strtolower($file_extension));
+
+    $icon_name = !empty($file_mimetype) ? preg_replace("/\//","-", $file_mimetype) : "unknown";
+
+    $icon_extension = (($size == "scalable") ? ".svg" : ".png");
+
+    return $icons_assets_path ."/". $size ."/". $icon_name . $icon_extension;
   }
+
+}
