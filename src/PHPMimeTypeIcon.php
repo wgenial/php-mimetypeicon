8b6ccaa075d4fdc7d55f07d1ff24717f3502b0a2
eldino-740 <68180174+eldino-740@users.noreply.github.com>
2020-09-14 13:32:59 +0200

    Update PHPMimeTypeIcon.php
    add test for icon exist and else return none.png

--- src/PHPMimeTypeIcon.php
+++ src/PHPMimeTypeIcon.php
@@ -18,8 +18,8 @@ class PHPMimeTypeIcon {
     $icon_name = !empty($file_mimetype) ? preg_replace("/\//","-", $file_mimetype) : "unknown";
 
     $icon_extension = (($size == "scalable") ? ".svg" : ".png");
-
-    return $icons_assets_path ."/". $size ."/". $icon_name . $icon_extension;
+    if(file_exist($icons_assets_path ."/". $size ."/". $icon_name . $icon_extension))return $icons_assets_path ."/". $size ."/". $icon_name . $icon_extension;
+    else return $icons_assets_path ."/". $size ."/none.png";
   }
 
 }
